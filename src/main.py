3dc4cf7b94ce3edb5e03bc691378707d96d7674c
makzuu <mr.greenmousth@gmail.com>
2024-03-13 23:23:17 -0300

    Add TextNode class and unit tests

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode
+
+def main():
+    textnode = TextNode("hello world", "bold")
+    print(textnode)
+
+main()
---
+++ src/test_textnode.py
@@ -0,0 +1,40 @@
+import unittest
+
+from textnode import TextNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+
+    def test_eq_false(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a different text node", "bold")
+
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "italic")
+
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", "bold", "archlinux.org")
+        node2 = TextNode("This is a text node", "bold", "boot.dev")
+
+        self.assertNotEqual(node, node2)
+
+    def test_default(self):
+        node = TextNode("This is a text node", "bold")
+        self.assertIsNone(node.url)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", "bold", "archlinux.org")
+        self.assertEqual("TextNode(This is a text node, bold, archlinux.org)", repr(node))
+
+
+if __name__ == "__main__":
+    unittest.main()

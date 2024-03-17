e3d014842145db31bcb2e32a1421c561e7f8c67e
makzuu <mr.greenmousth@gmail.com>
2024-03-16 22:02:12 -0300

    Add LeafNode class and it's tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,7 @@
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 import unittest
+
 
 class TestHTMLNode(unittest.TestCase):
     def test_tag_is_none(self):
@@ -19,6 +21,27 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode(tag="a", value="boot.dev", props={"href": "https://www.boot.dev"})
         expected = "HTMLNode(a, boot.dev, None, {'href': 'https://www.boot.dev'})"
         self.assertEqual(repr(node), expected)
+
+
+class TeastLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        expected = "<p>This is a paragraph of text.</p>"
+        self.assertEqual(node.to_html(), expected)
+
+    def test_to_html2(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        expected = "<a href=\"https://www.google.com\">Click me!</a>"
+        self.assertEqual(node.to_html(), expected)
+
+    def test_to_html3(self):
+        node = LeafNode()
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_to_html4(self):
+        node = LeafNode(value="This is a paragraph of text.")
+        expected = "This is a paragraph of text."
+        self.assertEqual(node.to_html(), expected)
 
 
 if __name__ == "__main__":

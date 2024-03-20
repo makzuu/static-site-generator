d7edfacc8f2e28739671c0b36fb2c1bb8b73bb10
makzuu <mr.greenmousth@gmail.com>
2024-03-20 17:47:35 -0300

    Add block_to_block_type function and it's tests

--- src/block.py
+++ src/block.py
@@ -1,3 +1,10 @@
+BLOCK_TYPE_PARAGRAPH = "paragraph"
+BLOCK_TYPE_HEADING = "heading"
+BLOCK_TYPE_CODE = "code"
+BLOCK_TYPE_QUOTE = "quote"
+BLOCK_TYPE_UNORDERED_LIST = "unordered_list"
+BLOCK_TYPE_ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
     return list(
             map(
@@ -8,3 +15,35 @@ def markdown_to_blocks(markdown):
                     )
                 )
             )
+
+def get_start(lines):
+    start = lines[0].split(" ")[0]
+    filter_function = lambda current: current == start
+    if start[:-1].isdigit() and start[-1] == ".":
+        filter_function = lambda current: current[:-1].isdigit() and current[-1] == "."
+    filtered_lines = filter(
+            filter_function,
+            map(
+                lambda line: line.split(" ")[0],
+                lines
+                )
+            )
+    if len(list(filtered_lines)) != len(lines):
+        return None
+    return start
+
+def block_to_block_type(block):
+    heading = block.split(" ")[0]
+    if heading.endswith("#") and len(heading) < 7:
+        return BLOCK_TYPE_HEADING
+    if block.startswith("```") and block.endswith("```"):
+        return BLOCK_TYPE_CODE
+    lines = block.split("\n")
+    start = get_start(lines)
+    if start == ">":
+        return BLOCK_TYPE_QUOTE
+    if start == "*" or start == "-":
+        return BLOCK_TYPE_UNORDERED_LIST
+    if start != None and start[:-1].isdigit() and start[-1] == ".":
+        return BLOCK_TYPE_ORDERED_LIST
+    return BLOCK_TYPE_PARAGRAPH
--- src/test_block.py
+++ src/test_block.py
@@ -1,5 +1,7 @@
 import unittest
-from block import markdown_to_blocks
+from block import (markdown_to_blocks, block_to_block_type,
+BLOCK_TYPE_PARAGRAPH, BLOCK_TYPE_HEADING, BLOCK_TYPE_CODE, BLOCK_TYPE_QUOTE,
+BLOCK_TYPE_UNORDERED_LIST, BLOCK_TYPE_ORDERED_LIST)
 
 class TestBlock(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -25,6 +27,47 @@ This is the same paragraph on a new line
     def test_markdown_to_blocks3(self):
         markdown = "\n## Heading     "
         self.assertEqual(markdown_to_blocks(markdown), ["## Heading"])
+
+    def test_block_to_block_type(self):
+        block = "# Title"
+        self.assertEqual(block_to_block_type(block), BLOCK_TYPE_HEADING)
+
+    def test_block_to_block_type2(self):
+        block = "#Title"
+        self.assertEqual(block_to_block_type(block), BLOCK_TYPE_PARAGRAPH)
+
+    def test_block_to_block_type3(self):
+        block = "###### Titulo 2"
+        self.assertEqual(block_to_block_type(block), BLOCK_TYPE_HEADING)
+
+    def test_block_to_block_type4(self):
+        block = "####### Titulo 2"
+        self.assertEqual(block_to_block_type(block), BLOCK_TYPE_PARAGRAPH)
+
+    def test_block_to_block_type5(self):
+        block = "```print(\"Hello World\")\nsome_function()```"
+        self.assertEqual(block_to_block_type(block), BLOCK_TYPE_CODE)
+
+    def test_block_to_block_type6(self):
+        block = "> some quote\n> another quote"
+        self.assertEqual(block_to_block_type(block), BLOCK_TYPE_QUOTE)
+
+    def test_block_to_block_type7(self):
+        block = "> some quote\n* another quote"
+        self.assertEqual(block_to_block_type(block), BLOCK_TYPE_PARAGRAPH)
+
+    def test_block_to_block_type8(self):
+        block = "* comprar pan\n* comprar bebida"
+        self.assertEqual(block_to_block_type(block), BLOCK_TYPE_UNORDERED_LIST)
+
+    def test_block_to_block_type9(self):
+        block = "- comprar pan\n- comprar bebida"
+        self.assertEqual(block_to_block_type(block), BLOCK_TYPE_UNORDERED_LIST)
+
+    def test_block_to_block_type10(self):
+        block = "1. list item\n2. another list item"
+        self.assertEqual(block_to_block_type(block), BLOCK_TYPE_ORDERED_LIST)
+
 
 if __name__ == "__main__":
     unittest.main()
